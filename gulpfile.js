05f94ba44548aca5fbbafd583579820498a7d7e7
Christian <christian.palazzo@aizoon.it>
2016-08-22 16:56:01 +0200

    angular app implementation

--- gulpfile.js
+++ gulpfile.js
@@ -53,10 +53,10 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'));
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest('vendor/jquery'));
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -66,7 +66,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('vendor/font-awesome'));
 });
 
 // Run everything
@@ -85,7 +85,7 @@ gulp.task('browserSync', function() {
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('js/**/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
---
+++ js/app/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Main module definition
+ */
+angular.module('appApp', [
+  'ui.router'
+])
+  .config(['$stateProvider', '$urlRouterProvider', _router]);
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+function _router($stateProvider, $urlRouterProvider, $breadcrumbProvider, gmLibraryProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+    .state('main', {
+      url: '/',
+      templateUrl: 'partials/main.html',
+      controller: 'MainCtrl as vm'
+    })
+
+    .state('medal', {
+      url: '/',
+      templateUrl: 'partials/medal.html',
+      controller: 'MainCtrl as vm'
+    })
+
+    .state('download', {
+      url: '/',
+      templateUrl: 'partials/download.html',
+      controller: 'MainCtrl as vm'
+    })
+
+    .state('contact', {
+      url: '/',
+      templateUrl: 'partials/contact.html',
+      controller: 'MainCtrl as vm'
+    });
+}
